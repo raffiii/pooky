47b4f4595b33ecff739d1eef5422553f041ea7b2
Rafael Baur <uounb@student.kit.edu>
2022-04-28 14:56:43 +0200

    Add structure to js side

--- static/structure.js
+++ static/structure.js
@@ -1,110 +1,155 @@
-use strict;
-
 current_id = 0;
 
-function Erase(box,color){
-    return {
-    type: "erase",
-        box
-        color: color,
-        id: ++current_id
-    };
+const erase_defaults = {
+    color: "#FFFFFF",
+    box: [0, 0, 0, 0],
+};
+
+class Erase {
+    constructor(erase) {
+        this.color = erase.color || erase_defaults.color;
+        this.box = erase.box || erase_defaults.box;
+    }
 }
 
-function ClipSrc(docname,pno,box,erase) {
-    return {
-        type: "clip_src",
-        box: box,
-        id: ++current_id
-    };
+const clip_src_defaults = {
+    doc: "",
+    pno: 0,
+    box: [0, 0, 0, 0],
+    erase: [],
+};
+
+class ClipSrc {
+    constructor(src) {
+        this.doc = src.doc || clip_src_defaults.doc;
+        this.pno = src.pno || clip_src_defaults.pno;
+        this.box = src.box || clip_src_defaults.box;
+        this.erase = src.erase || clip_src_defaults.erase;
+    }
 }
 
-function ClipDst(src_id,placing) {
-    return {
-        type: "clip_dst",
-        src_id: src_id,
-        placing: placing,
-        id: ++current_id
-    };
+const clip_dst_defaults = {
+    src: {},
+    box: [0, 0, 0, 0],
+};
+
+class ClipDst {
+    constructor(dst) {
+        this.src = dst.src || clip_dst_defaults.src;
+        this.box = dst.box || clip_dst_defaults.box;
+    }
 }
 
-function Image(docname,box) {
-    return {
-        type: "image",
-        docname: docname,
-        box: box,
-        id: ++current_id
-    };
+const image_defaults = {
+    src: "",
+    box: [0, 0, 0, 0],
+};
+
+class Image {
+    constructor(image) {
+        this.src = image.src || image_defaults.src;
+        this.box = image.box || image_defaults.box;
+    }
 }
 
-function Text(text,box,font,size,color,align,insertions) {
-    return {
-        type: "text",
-        text: text,
-        box: box,
-        font: font,
-        size: size,
-        color: color,
-        align: align,
-        insertions: insertions,
-        id: ++current_id
-    };
+const text_defaults = {
+    box: [0, 0, 0, 0],
+    text: "",
+    font: "",
+    size: 0,
+    color: "black",
+    align: "left",
+    insertions: [],
+};
+
+class Text {
+    constructor(text) {
+        this.box = text.box || text_defaults.box;
+        this.text = text.text || text_defaults.text;
+        this.font = text.font || text_defaults.font;
+        this.size = text.size || text_defaults.size;
+        this.color = text.color || text_defaults.color;
+        this.align = text.align || text_defaults.align;
+        this.insertions = text.insertions || text_defaults.insertions;
+    }
 }
 
-function Line(p,q) {
-    return {
-        type: "line",
-        p: p,
-        q: q,
-        id: ++current_id
-    };
+const line_defaults = {
+    p: [0, 0],
+    q: [0, 0],
+};
+
+class Line {
+    constructor(line) {
+        this.p = line.p || line_defaults.p;
+        this.q = line.q || line_defaults.q;
+    }
 }
 
-function Font(name,filename) {
-    return {
-        type: "font",
-        name: name,
-        filename: filename,
-        id: ++current_id
-    };
+const font_defaults = {
+    name: "",
+    filename: "",
+};
+
+class Font {
+    constructor(font) {
+        this.name = font.name || font_defaults.name;
+        this.filename = font.filename || font_defaults.filename;
+    }
 }
 
-function Page(template,inner){
-    return {
-        type: "page",
-        template: template,
-        inner: inner,
-        id: ++current_id
-    };
+const page_defaults = {
+    template: [],
+    inner: [0, 0, 0, 0],
+};
+
+class Page {
+    constructor(page) {
+        this.template = page.template || page_defaults.template;
+        this.inner = page.inner || page_defaults.inner;
+    }
 }
 
-function Part(page,first,content,name){
-    return {
-        type: "part",
-        page: page,
-        first: first,
-        content: content,
-        name: name,
-        id: ++current_id
-    };
+const part_defaults = {
+    page: {},
+    first: {},
+    content: [],
+    name: "",
+};
+
+class Part {
+    constructor(part) {
+        this.page = part.page || part_defaults.page;
+        this.first = part.first || part_defaults.first;
+        this.content = part.content || part_defaults.content;
+        this.name = part.name || part_defaults.name;
+    }
 }
 
-function info(parts,fonts,replaced,clips) {
-    return {
-        type: "info",
-        parts: parts,
-        fonts: fonts,
-        replaced: replaced,
-        clips: clips,
-        id: ++current_id
-    };
+const info_defaults = {
+    parts: [],
+    fonts: {},
+    replaced: {},
+    clips: {},
+};
+
+class Info {
+    constructor(info) {
+        this.parts = info.parts || info_defaults.parts;
+        this.fonts = info.fonts || info_defaults.fonts;
+        this.replaced = info.replaced || info_defaults.replaced;
+        this.clips = info.clips || info_defaults.clips;
+    }
 }
 
-function doc(info,files) {
-    return {
-        type: "doc",
-        info: info,
-        files: files,
-        id: ++current_id
-    };
+const doc_defaults = {
+    info: {},
+    files: {},
+};
+
+class Doc {
+    constructor(doc) {
+        this.info = new Info(doc.info || doc_defaults.info);
+        this.files = doc.files || doc_defaults.files;
+    }
 }
--- static/viewer.js
+++ static/viewer.js
@@ -1,33 +1,39 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+var url =
+    "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
-var pdfjsLib = window['pdfjs-dist/build/pdf'];
+var pdfjsLib = window["pdfjs-dist/build/pdf"];
 
 // The workerSrc property shall be specified.
-pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "//mozilla.github.io/pdf.js/build/pdf.worker.js";
 
 var pdfDoc = null,
     pageNum = 1,
     pageRendering = false,
     pageNumPending = null,
     scale = 0.8,
-    canvas = document.getElementById('the-canvas'),
-    overlay = document.getElementById('overlay'),
-    preview = document.getElementById('preview'),
-    ctx = canvas.getContext('2d'),
-    overlayCtx = overlay.getContext('2d'),
-    previewCtx = preview.getContext('2d'),
-    boxes = {};
+    canvas = document.getElementById("the-canvas"),
+    overlay = document.getElementById("overlay"),
+    preview = document.getElementById("preview"),
+    ctx = canvas.getContext("2d"),
+    overlayCtx = overlay.getContext("2d"),
+    previewCtx = preview.getContext("2d"),
+    boxes = {},
+    doc = {};
 
+function boxes() {
+    return doc.info.clips;
+}
 
-function drawBoxes(pno){
-    var ctx = overlay.getContext('2d');
+function drawBoxes(pno) {
+    var ctx = overlay.getContext("2d");
     ctx.clearRect(0, 0, overlay.width, overlay.height);
-    for (var box of boxes[pno]) {
+    for (var box of boxes()[pno]) {
         ctx.beginPath();
-        ctx.strokeStyle = 'red';
+        ctx.strokeStyle = "red";
         var x = box.x0,
             y = box.y0,
             w = box.x1 - box.x0,
@@ -43,42 +49,40 @@ function drawBoxes(pno){
  * @param num Page number.
  */
 function renderPage(num) {
-  pageRendering = true;
-  // Using promise to fetch the page
-  pdfDoc.getPage(num).then(function(page) {
-    var viewport = page.getViewport({scale: 1.});
-    scale = document.body.clientWidth / viewport.width *.48;
-    viewport = page.getViewport({scale: scale});
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-    overlay.height = viewport.height;
-    overlay.width = viewport.width;
-    preview.height = viewport.height;
-    preview.width = viewport.width;
+    pageRendering = true;
+    // Using promise to fetch the page
+    pdfDoc.getPage(num).then(function(page) {
+        var viewport = page.getViewport({ scale: 1 });
+        scale = (document.body.clientWidth / viewport.width) * 0.48;
+        viewport = page.getViewport({ scale: scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        overlay.height = viewport.height;
+        overlay.width = viewport.width;
+        preview.height = viewport.height;
+        preview.width = viewport.width;
 
+        // Render PDF page into canvas context
+        var renderContext = {
+            canvasContext: ctx,
+            viewport: viewport,
+        };
+        var renderTask = page.render(renderContext);
 
-    // Render PDF page into canvas context
-    var renderContext = {
-      canvasContext: ctx,
-      viewport: viewport
-    };
-    var renderTask = page.render(renderContext);
-
-    // Wait for rendering to finish
-    renderTask.promise.then(function() {
-      pageRendering = false;
-      if (pageNumPending !== null) {
-        // New page rendering is pending
-        renderPage(pageNumPending);
-        pageNumPending = null;
-      }
-      drawBoxes(pageNum-1);
-
+        // Wait for rendering to finish
+        renderTask.promise.then(function() {
+            pageRendering = false;
+            if (pageNumPending !== null) {
+                // New page rendering is pending
+                renderPage(pageNumPending);
+                pageNumPending = null;
+            }
+            drawBoxes(pageNum - 1);
+        });
     });
-  });
 
-  // Update page counters
-  document.getElementById('page_num').textContent = num;
+    // Update page counters
+    document.getElementById("page_num").textContent = num;
 }
 
 /**
@@ -86,87 +90,86 @@ function renderPage(num) {
  * finished. Otherwise, executes rendering immediately.
  */
 function queueRenderPage(num) {
-  if (pageRendering) {
-    pageNumPending = num;
-  } else {
-    renderPage(num);
-  }
+    if (pageRendering) {
+        pageNumPending = num;
+    } else {
+        renderPage(num);
+    }
 }
 
 /**
  * Displays previous page.
  */
 function onPrevPage() {
-  if (pageNum <= 1) {
-    return;
-  }
-  pageNum--;
-  queueRenderPage(pageNum);
+    if (pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
 }
-document.getElementById('prev').addEventListener('click', onPrevPage);
+document.getElementById("prev").addEventListener("click", onPrevPage);
 
 /**
  * Displays next page.
  */
 function onNextPage() {
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  queueRenderPage(pageNum);
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
 }
-document.getElementById('next').addEventListener('click', onNextPage);
+document.getElementById("next").addEventListener("click", onNextPage);
 
 /**
  * Asynchronously downloads PDF.
  */
- $('#source_file').on('change', function(e){
-  var file = e.target.files[0];
-  if(file == undefined)  {
-    return;
-  }
-  var fileReader = new FileReader();
+$("#source_file").on("change", function(e) {
+    var file = e.target.files[0];
+    if (file == undefined) {
+        return;
+    }
+    var fileReader = new FileReader();
 
-  fileReader.onload = function(e) {
-    var buffer = e.target.result;
-    var uint8Array = new Uint8Array(buffer);
-    pdfjsLib.getDocument(uint8Array).promise.then(function(pdfDoc_) {
-      pdfDoc = pdfDoc_;
-      $('#page_count').html(pdfDoc.numPages);
+    fileReader.onload = function(e) {
+        var buffer = e.target.result;
+        var uint8Array = new Uint8Array(buffer);
+        pdfjsLib.getDocument(uint8Array).promise.then(function(pdfDoc_) {
+            pdfDoc = pdfDoc_;
+            $("#page_count").html(pdfDoc.numPages);
 
-      // Initial/first page rendering
-      renderPage(pageNum);
-    });
-  };
+            // Initial/first page rendering
+            renderPage(pageNum);
+        });
+    };
 
-  fileReader.readAsArrayBuffer(file);
+    fileReader.readAsArrayBuffer(file);
+});
 
 
-  });
-
-  $('#load_bboxes').on('click', function() {
-    var file_data = $('#source_file').prop('files')[0];
+$("#load_bboxes").on("click", function() {
+    var file_data = $("#source_file").prop("files")[0];
     var form_data = new FormData();
-    form_data.append('file', file_data);
+    form_data.append("file", file_data);
     $.ajax({
-        url: '/util/bboxes', // <-- point to server-side PHP script
-        dataType: 'json',  // <-- what to expect back from the PHP script, if anything
+        url: "/util/bboxes", // <-- point to server-side PHP script
+        dataType: "json", // <-- what to expect back from the PHP script, if anything
         cache: false,
         contentType: false,
         processData: false,
         data: form_data,
-        type: 'post',
-        success: function(bboxes){
-            boxes = bboxes;
-            drawBoxes(pageNum-1);
-        }
-     });
+        type: "post",
+        success: function(bboxes) {
+            boxes() = bboxes;
+            drawBoxes(pageNum - 1);
+        },
+    });
 });
 
 var start = {};
 
-
-function getCoords(elem) { // crossbrowser version
+function getCoords(elem) {
+    // crossbrowser version
     var box = elem.getBoundingClientRect();
 
     var body = document.body;
@@ -178,7 +181,7 @@ function getCoords(elem) { // crossbrowser version
     var clientTop = docEl.clientTop || body.clientTop || 0;
     var clientLeft = docEl.clientLeft || body.clientLeft || 0;
 
-    var top  = box.top +  scrollTop - clientTop;
+    var top = box.top + scrollTop - clientTop;
     var left = box.left + scrollLeft - clientLeft;
 
     return { top: Math.round(top), left: Math.round(left) };
@@ -189,11 +192,10 @@ let get_overlay_coords = function(e) {
     let y = e.pageY;
     let coords = getCoords(overlay);
     return {
-    x: (x - coords.left) / scale,
-    y:(y - coords.top) / scale
+        x: (x - coords.left) / scale,
+        y: (y - coords.top) / scale,
     };
-
-}
+};
 
 let end_create_box = function(e) {
     let coords = get_overlay_coords(e);
@@ -201,11 +203,11 @@ let end_create_box = function(e) {
         x0: start.x,
         y0: start.y,
         x1: coords.x,
-        y1: coords.y
+        y1: coords.y,
     };
-    boxes[pageNum-1].push(box);
-    drawBoxes(pageNum-1);
-}
+    boxes[pageNum - 1].push(box);
+    drawBoxes(pageNum - 1);
+};
 
 let end_delete_box = function(e) {
     let coords = get_overlay_coords(e);
@@ -213,47 +215,59 @@ let end_delete_box = function(e) {
         x0: start.x,
         y0: start.y,
         x1: coords.x,
-        y1: coords.y
+        y1: coords.y,
     };
-    boxes[pageNum-1] = boxes[pageNum-1].filter(function(b) {
-        return !(b.x0 >= box.x0 && b.y0 >= box.y0 && b.x1 <= box.x1 && b.y1 <= box.y1);
+    boxes[pageNum - 1] = boxes[pageNum - 1].filter(function(b) {
+        return !(
+            b.x0 >= box.x0 &&
+            b.y0 >= box.y0 &&
+            b.x1 <= box.x1 &&
+            b.y1 <= box.y1
+        );
     });
-    drawBoxes(pageNum-1);
-}
+    drawBoxes(pageNum - 1);
+};
 
-let select = document.getElementById('select_origin_mode');
+let select = document.getElementById("select_origin_mode");
 
-overlay.addEventListener('mousedown', function(e) {
+overlay.addEventListener("mousedown", function(e) {
     start = get_overlay_coords(e);
 });
 
-overlay.addEventListener('mouseup', function(e) {
-    if(select.value == 'create') {
+overlay.addEventListener("mouseup", function(e) {
+    if (select.value == "create") {
         end_create_box(e);
-    } else if(select.value == 'delete') {
+    } else if (select.value == "delete") {
         end_delete_box(e);
     }
 });
 
-let container = document.getElementById('container');
+let container = document.getElementById("container");
 var active_box = null;
-container.addEventListener('click', function(e) {
-  var x = e.offsetX,
-      y = e.offsetY;
-  var page = pageNum-1;
-  active_box = boxes[page].find(function(b) {
-    return x > b.x0 && x < b.x1 && y > b.y0 && y < b.y1;
-  });
-
+container.addEventListener("click", function(e) {
+    var x = e.offsetX,
+        y = e.offsetY;
+    var page = pageNum - 1;
+    active_box = boxes[page].find(function(b) {
+        return x > b.x0 && x < b.x1 && y > b.y0 && y < b.y1;
+    });
 });
 
-container.addEventListener('blur', function(e) {
-  if (container.contains(document.activeElement)) {
-    return;
-  }
-  active_box = null;
+container.addEventListener("blur", function(e) {
+    if (container.contains(document.activeElement)) {
+        return;
+    }
+    active_box = null;
 });
 
-$('#container > *').on('focus', function(e) {
-  $('#container').focus();
+$("#container > *").on("focus", function(e) {
+    $("#container").focus();
+});
+
+document.getElementById("preset").addEventListener("change", function(e) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var data = JSON.parse(e.target.result);
+        document_info = data;
+    };
 });
